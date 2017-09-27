2ede84b45afbc867bc9627aafbfa834df8a44005
gristar <sheng.fighting@qq.com>
2017-09-27 15:03:06 +0800

    edit

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin')
@@ -9,6 +10,7 @@ const entry = {
 let htmlPlugin = [];
 Object.keys(entry).forEach(function (item) {
     htmlPlugin.push(new HtmlWebpackPlugin({
+        title: 'Pet',
         template: 'src/index.html',
         filename: item + '.html',
         chunks: ['vendor', item]
@@ -18,27 +20,44 @@ Object.keys(entry).forEach(function (item) {
 module.exports = {
     entry: entry,
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
                     }
-                }
-            ]
-        }]
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            publicPath: path.join(__dirname, '../dist/assets/'),
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.DefinePlugin({
-            WD_QQAPPID: JSON.stringify('1105309683'),
-            WD_WXAPPID: JSON.stringify('wxfdab5af74990787a')
+            LIBDIR: JSON.stringify('lib')
         }),
-
         new InlineManifestWebpackPlugin({
             name: 'webpackManifest'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
         })
     ].concat(htmlPlugin)
 }
